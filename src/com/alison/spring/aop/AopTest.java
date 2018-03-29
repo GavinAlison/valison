1fe5f34687c8dcb91607e1c85fec91f77984faff
Alison <921757697@qq.com>
2018-03-29 09:00:34 +0800

    asd

--- src/com/alison/spring/aop/AopTest.java
+++ src/com/alison/spring/aop/AopTest.java
@@ -1,7 +1,16 @@
 package com.alison.spring.aop;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AopTest {
+
 	public static void main(String[] args) {
-		System.out.println("asdasd");
+		String xmlPath = "com/alison/spring/aop/aop.xml";
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(xmlPath);
+//		HelloWorldImpl h1 = (HelloWorldImpl) ctx.getBean("h1");
+//		HelloWorldImpl2 h2 = (HelloWorldImpl2) ctx.getBean("h2");
+//		h1.method1();
 	}
+
 }
