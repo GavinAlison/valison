43a9ceb9478dcb3092eff5df6f99b032105171e7
Alison <921757697@qq.com>
2018-03-29 09:04:41 +0800

    1

--- src/com/alison/spring/aop/AopTest.java
+++ src/com/alison/spring/aop/AopTest.java
@@ -8,9 +8,9 @@ public class AopTest {
 	public static void main(String[] args) {
 		String xmlPath = "com/alison/spring/aop/aop.xml";
 		ApplicationContext ctx = new ClassPathXmlApplicationContext(xmlPath);
-//		HelloWorldImpl h1 = (HelloWorldImpl) ctx.getBean("h1");
-//		HelloWorldImpl2 h2 = (HelloWorldImpl2) ctx.getBean("h2");
-//		h1.method1();
+		// HelloWorldImpl h1 = (HelloWorldImpl) ctx.getBean("h1");
+		// HelloWorldImpl2 h2 = (HelloWorldImpl2) ctx.getBean("h2");
+		// h1.method1();
 	}
 
 }
