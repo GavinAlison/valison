265c9b67a058c950618c07f3a46aef7a47f3502b
GavinAlison <921757697@qq.com>
2018-03-31 14:55:39 +0800

    a

--- src/com/alison/poi/ExcelDemo.java
+++ src/com/alison/poi/ExcelDemo.java
@@ -1,9 +1,7 @@
 package com.alison.poi;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
