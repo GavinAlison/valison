50296b1c16749da410e679c739aebdbde6dc3b3c
Alison <921757697@qq.com>
2018-04-02 20:22:12 +0800

    12

--- src/com/alison/webservice/client/ClientTest.java
+++ src/com/alison/webservice/client/ClientTest.java
@@ -8,12 +8,13 @@ import javax.xml.ws.Service;
 
 import com.alison.webservice.service.impl.MyWebServiceImpl;
 
+// 用jax-ws开发的客户端会自动适应服务端的版本。
 public class ClientTest {
-
+	// TODO 待处理，出错了
 	public static void main(String[] args) throws MalformedURLException {
 		URL url = new URL("http://localhost:8080/valison/MyService?wsdl");
-		QName qname = new QName(
-				"http://impl.server.webservice.remote.ssm.com/",
+		// Qname是注册到服务中的
+		QName qname = new QName("http://impl.service.webservice.alison.com/",
 				"MyWebServiceImpl");
 		Service service = Service.create(url, qname);
 		MyWebServiceImpl serviceimpl = service.getPort(MyWebServiceImpl.class);
--- src/com/alison/webservice/service/impl/MyWebServiceImpl.java
+++ src/com/alison/webservice/service/impl/MyWebServiceImpl.java
@@ -4,8 +4,8 @@ import javax.jws.WebService;
 
 import com.alison.webservice.service.po.MyWebService;
 
+//加上这个注解，表示此实现类就是WebService服务
 @WebService
-// 加上这个注解，表示此实现类就是WebService服务
 public class MyWebServiceImpl implements MyWebService {
 	@Override
 	public String method01() {
