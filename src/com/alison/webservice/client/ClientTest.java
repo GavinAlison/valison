f0880a34ab84daf50c9d4702f5be490207f1d45c
alison <921757697@qq.com>
2018-04-03 23:20:44 +0800

    home

--- src/com/alison/webservice/client/ClientTest.java
+++ src/com/alison/webservice/client/ClientTest.java
@@ -1,16 +1,18 @@
 package com.alison.webservice.client;
 
-import java.net.MalformedURLException;
-
 import com.alison.webservice.out.service.MyWebService;
 import com.alison.webservice.out.service.MyWebServiceImplService;
 
 // 用jax-ws开发的客户端会自动适应服务端的版本。
 public class ClientTest {
 	// TODO 待处理，出错了
-	public static void main(String[] args) throws MalformedURLException {
-		ClientTest t = new ClientTest();
-		t.method01();
+	public static void main(String[] args) {
+		try {
+			ClientTest t = new ClientTest();
+			t.method01();
+		} catch (Exception e) {
+
+		}
 
 		// URL url = new URL("http://localhost:8080/valison/MyService?wsdl");
 		// // Qname是注册到服务中的
@@ -27,6 +29,8 @@ public class ClientTest {
 		MyWebService a = new MyWebServiceImplService()
 				.getMyWebServiceImplPort();
 		String str = a.method01("1111");
+		System.out.println("------------");
 		System.out.println(str);
+		System.out.println("------------");
 	}
 }
