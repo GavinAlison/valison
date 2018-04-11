c08ffb4c399fe3282caa7de2acf674e8ca6ae9f4
alison <921757697@qq.com>
2018-04-11 08:42:49 +0800

    1

--- src/com/alison/miance/ArrayTest.java
+++ src/com/alison/miance/ArrayTest.java
@@ -2,12 +2,22 @@ package com.alison.miance;
 
 import java.util.Random;
 
+/**
+ * 求出数组中重复次数大于num的数值<br>
+ * 一个int[] , length =5000, 数组元素取值范围: 0-500<br>
+ * 思路： map, 数组,
+ * 
+ * @author hy
+ * 
+ */
 public class ArrayTest {
-
+	// 原数组的长度
 	private static final int arrLength = 5000;
-
+	// 值数组的长度
 	private static final int valueLength = 500;
-
+	// 重复次数变量
+	private static final int num = 15;
+	// 生成随机函数的变量
 	private static Random random = new java.util.Random();
 
 	/**
@@ -18,23 +28,49 @@ public class ArrayTest {
 	/**
 	 * 存储重复的数组
 	 */
-	private static int[] countArr = new int[arrLength];
+	private static int[] countArr = new int[valueLength];
+
+	public ArrayTest() {
+		initArr();
+	}
 
 	public void initArr() {
-		for (int i = 0; i <= arrLength; i++) {
-			arr[i] = (int) random.nextInt(arrLength);
+		for (int i = 0; i < arrLength; i++) {
+			arr[i] = random.nextInt(valueLength);
+		}
+	}
+
+	public void printArr(int[] arr) {
+		for (int i = 0; i < arrLength; i++) {
+			System.out.print(arr[i] + ",");
 		}
 	}
 
 	/**
-	 * 求出数组中重复次数大于3的数值<br>
+	 * 求出数组中重复次数大于num的数值<br>
 	 * 一个int[] , length =5000, 数组元素取值范围: 0-500<br>
-	 * 思路： map, 数组
+	 * 思路： map, 数组,
 	 */
 	public void method01() {
 		for (int i = 0; i < arrLength; i++) {
 			int temp = arr[i];
-
+			countArr[temp]++;
+			// System.out.print("countArr[" + temp + "]=" + countArr[temp] +
+			// "");
 		}
+		for (int i = 0; i < valueLength; i++) {
+			if (countArr[i] > num) {
+				System.out.print("countArr[" + i + "]:" + countArr[i] + ",");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		ArrayTest a = new ArrayTest();
+		a.printArr(arr);
+		System.out.println();
+		System.out.println("------------");
+		a.method01();
+		System.out.println();
 	}
 }
